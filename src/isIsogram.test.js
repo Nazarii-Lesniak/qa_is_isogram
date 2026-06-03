b498075f064c1f91eae6f9e534a7c386cecdae33
Nazarii-Lesniak <naziksay@gmail.com>
2026-06-03 11:33:28 +0300

    test: add unit tests for isIsogram

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,20 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`returns true if all letters are different`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it(`returns false if string contains duplicate letters`, () => {
+    expect(isIsogram('look')).toBe(false);
+  });
+
+  it(`returns false if there are duplicate letters in different case`, () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it(`returns true if the string is empty`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
 });
